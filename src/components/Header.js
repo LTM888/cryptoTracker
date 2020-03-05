e0a406b77ae8dd363b3f2a3eb557f744e7ca1639
Leigh Martin <leighmartin@Leighs-MacBook-Pro.local>
2020-03-04 22:54:37 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = () => {
+    return (
+        <View style={header}>
+            <Text style={header}>
+                LTM Crypto App
+            </Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    headerContainer: {
+        display:'flex',
+        marginTop: 55,
+        alignItems: 'center'
+    },
+
+    header: {
+        fontWeight:'bold',
+        fontSize: 20.
+    }
+})
+
+const {headerContainer, header} = style;
+
+
+export default Header;
